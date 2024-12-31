573116be3b13e5345fb666a6eef55ae54090d6aa
sara <edvin.ohrstrom@gmail.com>
2024-12-31 01:52:11 +0100

    Added some old classes from WF Steelmeet

---
+++ SteelmeetWPF/Fullscreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SteelmeetWPF
+{
+    internal class Fullscreen
+    {
+        public void ToggleFullscreen( bool _isFullscreen, Window _window)
+        {
+            if ( _isFullscreen )
+                _window.WindowState = WindowState.Normal;
+            else
+                _window.WindowState = WindowState.Maximized;
+        }
+    }
+}
