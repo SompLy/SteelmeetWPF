79884fd581fab6893f3b205e6fffce4b259ea70c
sara <edvin.ohrstrom@gmail.com>
2024-12-31 01:55:11 +0100

    Click and drag to move window with tabcontrol & updated tabcontrol style

---
+++ SteelmeetWPF/TabControlStyleCustom.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace SteelmeetWPF
+{
+    public partial class TabControlStyleCustom : ResourceDictionary
+    {
+        public TabControlStyleCustom()
+        {
+            InitializeComponent();
+        }
+
+        private void TabHeader_MouseLeftButtonDown( object sender, System.Windows.Input.MouseButtonEventArgs e )
+        {
+            // Find the parent window
+            if( e.LeftButton == MouseButtonState.Pressed )
+            {
+                Window window = Window.GetWindow((DependencyObject)sender);
+                window?.DragMove();
+            }
+        }
+    }
+}
