5bc462c6d925c689239106bf29975951fa5d16e0
Edvin Öhrström <edviohrs@playgroundsquad.com>
2025-01-29 17:17:53 +0100

    Created a bunch of UserControls for Control Window and Spectator Window

---
+++ SteelmeetWPF/JudgeControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SteelmeetWPF
+{
+    /// <summary>
+    /// Interaction logic for JudgeControl.xaml
+    /// </summary>
+    public partial class JudgeControl : UserControl
+    {
+        public JudgeControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
