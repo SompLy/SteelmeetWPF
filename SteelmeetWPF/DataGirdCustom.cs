d394619f5e5862a74d7193a70112f875e7a1105e
Edvin Öhrström <edviohrs@playgroundsquad.com>
2025-02-16 17:51:14 +0100

    Added Custom DataGrid & updated DgFormat classes to set custom headers in datagrids dynamically

---
+++ SteelmeetWPF/DataGirdCustom.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace SteelmeetWPF
+{
+    class DataGirdCustom : DataGrid
+    {
+        private readonly HashSet<string> _hiddenColumns = new(); // Stores hidden columns dynamically
+
+        public DataGirdCustom()
+        {
+            this.AutoGeneratingColumn += OnAutoGeneratingColumn;
+        }
+
+        private void OnAutoGeneratingColumn(object? sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            if (_hiddenColumns.Contains(e.PropertyName))
+            {
+                e.Cancel = true;
+                return;
+            }
+
+            PropertyInfo? property = typeof(ControlDgFormat).GetProperty(e.PropertyName); ;
+            if (sender is DataGrid dataGrid)
+            {            
+                if( dataGrid.Name == "controlDg" )
+                {
+                    property = typeof( ControlDgFormat ).GetProperty( e.PropertyName );
+                }
+                else if( dataGrid.Name == "weightInDg")
+                {
+                    property = typeof( WeighInDgFormat ).GetProperty( e.PropertyName );
+                }
+                //else if( sender == spectatorDg )
+                //{
+                //    property = typeof( SpectatorDgFormat ).GetProperty( e.PropertyName );
+                //}
+            }
+            
+            var displayAttr = property?.GetCustomAttribute<DisplayAttribute>();
+            if (displayAttr != null)
+                e.Column.Header = displayAttr.Name;
+        }
+        public void ToggleColumnVisibility(string columnName, bool isVisible)
+        {
+            var column = this.Columns.FirstOrDefault(c => c.Header?.ToString() == columnName);
+            if (column != null)
+                column.Visibility = isVisible ? Visibility.Visible : Visibility.Collapsed;
+            else if (!isVisible)
+                _hiddenColumns.Add(columnName);
+        }
+    }
+}
---
+++ SteelmeetWPF/WeighInDgFormat.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SteelmeetWPF
+{
+    public class WeighInDgFormat
+    {
+        [Display( Name = "Group" )]
+        public string? groupNumber { get; set; }
+
+        [Display( Name = "Name" )]
+        public string? name { get; set; }
+
+        [Display( Name = "Lot" )]
+        public string? lotNumber { get; set; }
+
+        [Display( Name = "Weight Class" )]
+        public string? weightClass { get; set; }
+
+        [Display( Name = "Category" )]
+        public string? category { get; set; }
+
+        [Display( Name = "Licence Number" )]
+        public string? licenceNumber { get; set; }
+
+        [Display( Name = "Association" )]
+        public string? accossiation { get; set; }
+
+        [Display( Name = "Body Weight" )]
+        public string? bodyWeight { get; set; }
+
+        [Display( Name = "Squat Height" )]
+        public string? squatHeight { get; set; }
+
+        [Display( Name = "Tilted" )]
+        public string? tilted { get; set; }
+
+        [Display( Name = "Squat" )]
+        public string? s1 { get; set; }
+
+        [Display( Name = "Bench Height" )]
+        public string? benchHeight { get; set; }
+
+        [Display( Name = "Bench Rack" )]
+        public string? benchRack { get; set; }
+
+        [Display( Name = "Liftoff" )]
+        public string? liftoff { get; set; }
+
+        [Display( Name = "Bench" )]
+        public string? b1 { get; set; }
+
+        [Display( Name = "Deadlift" )]
+        public string? d1 { get; set; }
+    }
+}
