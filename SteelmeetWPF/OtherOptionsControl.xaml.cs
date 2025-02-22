20571c3d15d5fadadfc3a2ec1fff459e03ffa9f6
sara <edvin.ohrstrom@gmail.com>
2025-02-23 00:55:16 +0100

    Added Other optionsControl Usercontrol

---
+++ SteelmeetWPF/OtherOptionsControl.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SteelmeetWPF
+{
+    /// <summary>
+    /// Interaction logic for OtherOptionsControl.xaml
+    /// </summary>
+    public partial class OtherOptionsControl : UserControl
+    {
+        public OtherOptionsControl()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenSpectatorBtn_Click( object sender, RoutedEventArgs e )
+        {
+
+        }
+    }
+}
