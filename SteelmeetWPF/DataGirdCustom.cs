e8598c6b49bfe54c611960eb9db3b6b155172671
lillabofinken <pbkrogen65@gmail.com>
2025-04-28 00:29:33 +0200

    WIP Auto Font Scale

--- SteelmeetWPF/DataGirdCustom.cs
+++ SteelmeetWPF/DataGirdCustom.cs
@@ -7,6 +7,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows;
+using System.Windows.Data;
+using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.Windows.Controls.Primitives;
+using System.Windows.Threading;
 
 namespace SteelmeetWPF
 {
@@ -17,6 +22,10 @@ namespace SteelmeetWPF
         public DataGirdCustom()
         {
             this.AutoGeneratingColumn += OnAutoGeneratingColumn;
+
+            //this.Loaded += ( s, e ) => FontAutoScale();
+            //this.SizeChanged += ( s, e ) => FontAutoScale();
+
         }
 
         private void OnAutoGeneratingColumn(object? sender, DataGridAutoGeneratingColumnEventArgs e)
@@ -55,6 +64,53 @@ namespace SteelmeetWPF
                 column.Visibility = isVisible ? Visibility.Visible : Visibility.Collapsed;
             else if (!isVisible)
                 _hiddenColumns.Add(columnName);
+        }
+
+        private void FontAutoScale()
+        {
+            if (this.Columns == null || this.Columns.Count == 0)
+                return;
+
+            AutoSizeColumns();
+            FontSizeCalculation();
+        }
+        private void FontSizeCalculation()
+        {
+            double totalWidth = this.ActualWidth;
+            double dataGridWidth = GetDataGridActualWidth();
+
+            double multiplier = totalWidth / dataGridWidth;
+
+            this.FontSize *= multiplier;
+        }
+        private double GetDataGridActualWidth()
+        {
+            double width = 0;
+            for (int i = 0; i < this.Columns.Count; i++)
+                width += this.Columns [i].ActualWidth;
+            return width;
+        }
+      
+        public void AutoSizeColumns()
+        {
+            double minWidth = 0;
+
+            foreach (var column in this.Columns)
+            {
+                column.Width = new DataGridLength( 1, DataGridLengthUnitType.SizeToCells );
+                //column.Width = new DataGridLength( 1, DataGridLengthUnitType.SizeToHeader );
+                //column.Width = new DataGridLength( 1, DataGridLengthUnitType.Auto );
+            }
+
+            this.Dispatcher.BeginInvoke
+                ( System.Windows.Threading.DispatcherPriority.ContextIdle, new Action( () =>
+            {
+                    foreach (var column in this.Columns)
+                    {
+                        if (column.ActualWidth < minWidth)
+                            column.Width = new DataGridLength( minWidth, DataGridLengthUnitType.Pixel );
+                    }
+            } ) );
         }
     }
 }
